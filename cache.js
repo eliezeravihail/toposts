cd884b0d2a41b901ff0b2c8d75ee2ef1c6d0fcbc
eliezer avihail <eliezeravihail@gmail.com>
2021-04-26 01:48:54 +0300

    fix to test range

--- cache.js
+++ cache.js
@@ -1,36 +1,48 @@
 
-
-const fillZeros_byRows = (arr2d) => {
+const readByRows = (arr2d) => {
     for (let i = 0; i < arr2d.length; i++) {
         for (let j = 0; j < arr2d[0].length; j++) {
-            arr2d[i][j] = "0";
+            arr2d[i][j];
         }
     }
 }
 
-const fillZeros_byColumns = (arr2d) => {
+const readByColumns = (arr2d) => {
     for (let i = 0; i < arr2d[0].length; i++) {
         for (let j = 0; j < arr2d.length; j++) {
-            arr2d[j][i] = "0";
+            arr2d[j][i];
         }
     }
 }
 
 const testTime = (func, arg) => {
     timestart = (new Date()).getTime()
-    for (let index = 0; index < 1000; index++) {
+    for (let index = 0; index < 100; index++) {
         func(arg);
     }
-    console.log(func.name,": ",((new Date()).getTime() - timestart)/1000.0);
-}
-
-const arr2d = new Array(100);
-for (let i = 0; i < 100; i++) {
-        arr2d[i] = new Array(100).fill(0);
+    return (new Date().getTime() - timestart)/100.0;
 }
 
 
-testTime(fillZeros_byRows,arr2d);
-testTime(fillZeros_byColumns,arr2d);
+const findChacheLimit = (testFunc, rowsFunc,ColumnsFunc)=>{
+    let limit = 10;
+    let maxTest = 100000;
+    const gump = 10;
+    const result = new Array(maxTest/gump);
+    while (limit<maxTest){
+        const tempArr2d = new Array(limit);
+        for (let i = 0; i < limit; i++) {
+            tempArr2d[i] = new Array(limit).fill(0);
+        }
+        result.push(`line's length: ${limit}\t rows: ${testFunc(rowsFunc,tempArr2d)}\tcolumns: ${testFunc(ColumnsFunc,tempArr2d)}`);
+        limit *= gump;
+    }
+    
+    result.forEach(x=>console.log(x))
+
+}
+
+findChacheLimit(testTime, readByRows,readByColumns)
+
 
 
